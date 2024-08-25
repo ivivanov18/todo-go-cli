6400a575686010a5a2fdf887a0176b86619dee84
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2024-08-26 00:14:15 +0300

    feature: - add function GetAllTasks which reads and parses the csv file into tasks - add function to Display the tasks

--- operations/list_tasks.go
+++ operations/list_tasks.go
@@ -2,6 +2,7 @@ package operations
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/ivivanov18/todo-go-cli/types"
@@ -15,4 +16,29 @@ func ListTasks() {
 		task := strings.Split(record[0], ";")
 		fmt.Printf("%d - %s | %s | %s\n", i+1, task[0], task[1], task[2])
 	}
+}
+
+func DisplayTasks() {
+	tasks := GetAllTasks()
+
+	for i, task := range tasks {
+		fmt.Printf("%d - %s | %s | %v\n", i+1, task.Name, task.DueDate, task.Done)
+	}
+}
+
+func GetAllTasks() []types.Task {
+	records := utils.ReadFile(types.TASKS_FILENAME)
+	tasks := make([]types.Task, len(records))
+
+	for i, record := range records {
+		task := strings.Split(record[0], ";")
+		done, _ := strconv.ParseBool(task[2])
+		tasks[i] = types.Task{
+			Name:    task[0],
+			DueDate: task[1],
+			Done:    done,
+		}
+	}
+
+	return tasks
 }
